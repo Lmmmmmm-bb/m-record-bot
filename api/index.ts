425cf56bdb0691293defb60a59f3677ff086a1d3
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-20 00:50:14 +0800

    chore: clean up

--- api/index.ts
+++ api/index.ts
@@ -2,5 +2,5 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default (request: VercelRequest, response: VercelResponse) => {
   const { name = 'lmmmmmm' } = request.query;
-  return response.send(`Hello ${name}`);
+  return response.send(`Hello ${name}, it is lz-m-record bot.`);
 };
--- bot/utils.ts
+++ bot/utils.ts
@@ -1,7 +1,8 @@
+import dayjs from 'dayjs';
+
 import { MRecord } from './types';
 import { supabase } from './supa';
 import { SUPABASE_RECORDS } from './config';
-import dayjs from 'dayjs';
 
 export const formatRecords = (records: MRecord[]) => {
   const recordsDate = records.map((item) => item.date);
