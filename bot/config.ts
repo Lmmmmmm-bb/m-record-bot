e17a5f6f969276e4716140ed15dae81b02694143
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-19 13:08:18 +0800

    feat: history record

--- bot/config.ts
+++ bot/config.ts
@@ -4,3 +4,10 @@ export const webhook = {
   domain: process.env.BOT_WEBHOOK_DOMAIN || '',
   hookPath: process.env.BOT_WEBHOOK_PATH || ''
 };
+
+export const supabaseConfig = {
+  url: process.env.SUPA_BASE_URL || '',
+  key: process.env.SUPA_BASE_KEY || ''
+};
+
+export const SUPABASE_RECORDS = 'records';
--- bot/index.ts
+++ bot/index.ts
@@ -1,11 +1,19 @@
 import { Telegraf } from 'telegraf';
 
-import { botToken, webhook } from './config';
+import { supabase } from './supa';
+import { MRecord } from './types';
+import { botToken, SUPABASE_RECORDS, webhook } from './config';
 
 export const bot = new Telegraf(botToken);
 
 bot.start((ctx) => {
   ctx.reply('Welcome to _lmmmmmm\'s bot.');
+});
+
+bot.command('history', async (ctx) => {
+  const result = await supabase.from(SUPABASE_RECORDS).select();
+  const records: MRecord[] = result.data ?? [];
+  ctx.replyWithMarkdownV2(records.map((item) => `**${item.date}**`).join('\n'));
 });
 
 bot.launch({ webhook });
