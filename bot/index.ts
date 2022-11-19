f45910458f6ce1344b8269b35ff317db40780c01
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-11-19 16:04:47 +0800

    feat: `/recent` command support

--- bot/index.ts
+++ bot/index.ts
@@ -33,9 +33,28 @@ bot.command('record', async (ctx) => {
     const reply = `前三个月的规律为 -> ${dateDiffByDay(two, three)}-${dateDiffByDay(one, two)}-${dateDiffByDay(newRecord.date, one)}`;
 
     ctx.replyWithMarkdownV2(reply);
-  } catch(error) {
+  } catch (error) {
     console.log(`添加数据出错 ${error.message}`);
     ctx.reply(`添加数据出错 ${error.message}`);
+  }
+});
+
+// 最近五条数据
+bot.command('recent', async (ctx) => {
+  try {
+    const records = await getAllRecords();
+    const recentRecords = records.slice(-6);
+    const content: string[] = [];
+
+    for (let i = 0; i < recentRecords.length; i += 2) {
+      const f = recentRecords[i];
+      const s = recentRecords[i + 1];
+      content.push(`${f.date} - ${s.date} -> 距离 ${dateDiffByDay(f.date, s.date)}`);
+    }
+    ctx.replyWithMarkdownV2(content.join('\n'));
+  } catch (error) {
+    console.log(`查找数据出错 ${error.message}`);
+    ctx.reply(`查找数据出错 ${error.message}`);
   }
 });
 
