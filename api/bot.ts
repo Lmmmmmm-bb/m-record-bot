4921686d484be12b93c895de7c2ad6e8600cb639
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-01-08 16:32:10 +0800

    revert: edge functions

--- api/bot.ts
+++ api/bot.ts
@@ -2,10 +2,6 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 import { bot } from '../bot';
 
-export const config = {
-  runtime: 'edge'
-};
-
 export default async (request: VercelRequest, response: VercelResponse) => {
   console.log('[bot] request body', request.body);
   try {
